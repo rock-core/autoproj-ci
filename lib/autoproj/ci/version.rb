57bea98c7e78b98a8a2b49b33d6e76601fda57c5
Sylvain <sylvain.joyeux@tidewise.io>
2019-08-30 19:54:34 -0300

    release 0.2.0

--- lib/autoproj/ci/version.rb
+++ lib/autoproj/ci/version.rb
@@ -2,6 +2,6 @@ module Autoproj
     # autoproj-ci is an Autoproj plugin to provide functionality useful for CI
     # (automated builds & tests)
     module CI
-        VERSION = "0.1.0"
+        VERSION = "0.2.0"
     end
 end
