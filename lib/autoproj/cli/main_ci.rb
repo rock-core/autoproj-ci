a27ea6f7030033f8a6caf10accc960effeca68d1
Sylvain <sylvain.joyeux@tidewise.io>
2021-10-26 20:09:55 -0300

    fix(cli): add missing `boolean` type on process-test-results --force

--- lib/autoproj/cli/main_ci.rb
+++ lib/autoproj/cli/main_ci.rb
@@ -54,7 +54,8 @@ module Autoproj
 
             desc "process-test-results [ARGS]",
                  "Process test output (assumed to be in JUnit XML) through xunit-viewer"
-            option :force, desc: "re-generates existing output", default: false
+            option :force, desc: "re-generates existing output",
+                           default: false, type: :boolean
             option :xunit_viewer, desc: "path to xunit-viewer", default: "xunit-viewer"
             def process_test_results
                 require "autoproj/cli/ci"
