e722eb16678e642450e03bb5cd321391bb0f1df4
Sylvain <sylvain.joyeux@tidewise.io>
2024-08-18 08:49:32 -0300

    fix: make sure the root of created tarball artifacts are go+rx
    The code is using mktmpdir to create the folder, which makes it 0700.
    The resulting tarball has a root folder with the same permissions, which
    breaks a system for all users except root when unpacked.

--- lib/autoproj/cli/standalone_ci.rb
+++ lib/autoproj/cli/standalone_ci.rb
@@ -19,6 +19,8 @@ module Autoproj
                    type: :string, default: nil
             def rebuild_root(config_dir, cache_root, output)
                 dir = Dir.mktmpdir
+                FileUtils.chmod 0755, dir
+
                 Autoproj::CI::Rebuild.prepare_synthetic_buildroot(
                     File.join(config_dir, "installation-manifest"),
                     File.join(config_dir, "versions.yml"),
